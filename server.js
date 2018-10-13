fc1762336114efc3a3a94ce314a333409d4c1653
= <darlannakamura@hotmail.com>
2018-10-12 21:50:47 -0300

    Initial Commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var app = require('express')();
+
+let bodyParser = require("body-parser")
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+
+let port = process.env.PORT || 5000;
+app.listen(port, function(){
+    console.log("port at: "+ port);
+});
+module.exports = app;
